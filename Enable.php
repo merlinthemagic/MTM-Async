06f3d21dcbab47514477a70eab33eb70de867d17
merlinthemagic <github@martinpetermadsen.com>
2019-09-20 13:38:10 +0100

    initial with dependencies

--- Enable.php
+++ Enable.php
@@ -1,31 +1,24 @@
 <?php
 //© 2019 Martin Peter Madsen
 if (defined("MTM_ASYNC_BASE_PATH") === false) {
-	define('MTM_ASYNC_VENDOR_NAME', basename(realpath(__DIR__ . DIRECTORY_SEPARATOR . "..")));
-	define("MTM_ASYNC_BASE_NAME", basename(__DIR__));
 	define("MTM_ASYNC_BASE_PATH", __DIR__ . DIRECTORY_SEPARATOR);
-	define('MTM_ASYNC_CLASS_PATH', implode(DIRECTORY_SEPARATOR, array_slice(explode(DIRECTORY_SEPARATOR, MTM_ASYNC_BASE_PATH), 0, -3)) . DIRECTORY_SEPARATOR);
-	spl_autoload_register(function($className) {
+	spl_autoload_register(function($className)
+	{
 		if (class_exists($className) === false) {
-			$cPath	= array_values(array_filter(explode("\\", $className)));
-			if ($cPath[0] == MTM_ASYNC_VENDOR_NAME && $cPath[1] == MTM_ASYNC_BASE_NAME) {
-				$filePath	= MTM_ASYNC_CLASS_PATH . str_replace('\\', DIRECTORY_SEPARATOR, $className) . ".php";
-				if (is_readable($filePath) === true) {
-					require_once $filePath;
+			$cPath		= array_values(array_filter(explode("\\", $className)));
+			if (array_shift($cPath) == "MTM") {
+				if (array_shift($cPath) == "Async") {
+					$filePath	= MTM_ASYNC_BASE_PATH . implode(DIRECTORY_SEPARATOR, $cPath) . ".php";
+					if (is_readable($filePath) === true) {
+						require_once $filePath;
+					}
 				}
 			}
 		}
 	});
 	function loadMtmAsync()
 	{
-		$basePath		= realpath(__DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "..") . DIRECTORY_SEPARATOR;
-		$deps			= array();
-		$deps["MTM"]	= array("Utilities");
-		foreach ($deps as $dir => $libs) {
-			foreach ($libs as $name) {
-				require_once $basePath . $dir . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . "Enable.php";
-			}
-		}
+
 	}
 	loadMtmAsync();
 }
